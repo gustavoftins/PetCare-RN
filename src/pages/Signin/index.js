a9d375dc6ab928d6045d2e0ba7c56f7ab5c2a2c9
RubensKj <rubenskleinschmidtjr@gmail.com>
2019-09-05 14:50:12 -0300

    Login method with api

--- src/pages/Signin/index.js
+++ src/pages/Signin/index.js
@@ -19,21 +19,57 @@ export default function Signin({ navigation }) {
     const [error, setError] = useState('');
 
     async function handleLogin() {
-        const { email, password } = user;
 
-        try{
-            const response = await api.post("/auth/login", JSON.stringify(user))
-        }catch(err){
-            alert(err)
-        }
+       const { email, password } = user;
+
+       if(email == '' || password == ''){
+           setError("Preencha todos os campos");
+           return;
+       }else{
+          if(!(user.email.includes('@') && user.email.includes('.com'))){
+              setError("E-mail não válido");
+              return;
+          }
+       }
+
+       if(password.length > 100) {
+           setError("Senha não válida");
+           return;
+       }
+
+       await api.post("/auth/login", JSON.stringify(user)).then(response => {
+           const { token, user } = response.data
+           AsyncStorage.multiSet([
+               ['token', token],
+               ['user', user]
+           ])
+           navigation.navigate("Home");
+       }).catch(error => {
+           switch(error.message){
+               case "Network Error":
+                   return setError("O servidor está temporariamente desligado");
+                case "Request failed with status code 404":
+                    return setError("Não existe nenhuma empresa associada a este email.");
+                case "Request failed with status code 401":
+                    return setError("Email ou senha incorretos.");
+                default:
+                    return setError("");
+           } 
+       })
+
+
+
     }
     return (
         <View style={styles.container}>
             <Image 
                 source={require('../../assets/dog.png')}
             />
-            <NewInput placeholder="E-mail" onChangeText={(text) => setUser({email:text})}/>
-            <NewInput placeholder="Senha" onChangeText={(text) => setUser({password:text})}
+            <View>
+                <Text>{error}</Text>
+            </View>
+            <NewInput placeholder="E-mail" onChangeText={(text) => setUser({ ...user, email: text})}/>
+            <NewInput placeholder="Senha" onChangeText={(text) => setUser({ ...user, password: text})}
             secureTextEntry={true} />
             <NewButton text="Entrar"
                 onPress={handleLogin}
